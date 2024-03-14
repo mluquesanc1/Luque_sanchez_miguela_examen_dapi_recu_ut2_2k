8f7ca8a7b1ae66b7ea5d54696dd9610363ac01cb
mluquesanc1 <mluquesanc1@educacion.navarra.es>
2024-03-14 15:51:55 +0100

    jug

---
+++ examen ejercicio1.py
@@ -0,0 +1,10 @@
+def Ordenalista(numero1,numero2): 
+    print(numero1+numero2)
+  
+
+Ordenalista(2,3)
+Ordenalista(4,3)
+
+def NumeroPaar(marta,pepe,luis):
+     return  marta+pepe+luis
+print(NumeroPaar(2,2,2))
---
+++ examen ejercicio2.py
@@ -0,0 +1,8 @@
+fruta = {"platanos":0.74, "peras", 1.20, "pan", 1,20, } 
+CANTIDAD[] #DICCIONARIO VACIO
+cantidad = int(input("cuanta cantidad has consumido"))
+cantidad = fruta
+for i in futa: #BUCLE FOR
+    if (i==cantidad)
+    cantidaddes(f"tu futa es{fruta})
+    print("cantidad" [i])
